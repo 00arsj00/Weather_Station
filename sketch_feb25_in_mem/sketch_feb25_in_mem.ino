630022ef002ee3982206f00f91623328c2e60a5c
00arsj00 <alvaroraul.santos@gmail.com>
2024-03-05 22:03:09 -0600

    se actualizo el script de inicializador de memoria y comento

--- sketch_feb25_in_mem/sketch_feb25_in_mem.ino
+++ sketch_feb25_in_mem/sketch_feb25_in_mem.ino
@@ -1,9 +1,12 @@
 //librerias
 #include <AT24CX.h>
 //objetos
+//se utiliza lamemoria AT24C256
+//tiene 256kbits
+//32768 bytes
 AT24C256 mem;
 //variables
-int dir = 32765;
+int dir = 32765;//direccion donde se grabara el dato
 int dato;
 
 void setup() {
